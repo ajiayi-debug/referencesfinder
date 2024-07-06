98315ed089654471d82cd4a1eb5c8a3b266b4f24
ajiayi-debug <e0774928@u.nus.edu>
2024-07-06 15:39:21 +0800

    I MANAGED TO ACCESS INTERNET WITH GPT YAYAYAY

--- gptapi.py
+++ gptapi.py
@@ -10,16 +10,15 @@ az_path=os.getenv("az_path")
 result = subprocess.run([az_path, 'account', 'get-access-token', '--resource', 'https://cognitiveservices.azure.com', '--query', 'accessToken', '-o', 'tsv'], stdout=subprocess.PIPE)
 token = result.stdout.decode('utf-8').strip()
 
-# os.environ['OPENAITOKEN'] = token
-
 os.environ['AZURE_OPENAI_ENDPOINT'] = os.getenv('endpoint')
 os.environ['AZURE_OPENAI_API_KEY'] = token
+
 
 # Initialize the AzureOpenAI client
 client = AzureOpenAI(
     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"), 
     api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
-    api_version="2024-02-01"
+    api_version=os.getenv("ver")
 )
 
 
