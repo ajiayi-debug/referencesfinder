63fdd5ced49f6fc33cdb2709772e4e471d7eb638
ajiayi-debug <e0774928@u.nus.edu>
2024-07-15 14:45:03 +0800

    trying RAG

--- RAG.py
+++ RAG.py
@@ -1,20 +1,9 @@
-import openai
 import os
-from llama_index.llms import AzureOpenAI
-from llama_index.embeddings import AzureOpenAIEmbedding
-from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext, set_global_service_context
+from openai import AzureOpenAI
 from dotenv import load_dotenv
-import pypdf
 import subprocess
-from llama_index.llms.azure_openai import AzureOpenAI
-from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-import logging
-import sys
-from llama_index.core import Settings
 
 load_dotenv()
-
 
 az_path = os.getenv("az_path")
 
@@ -26,49 +15,19 @@ token = result.stdout.decode('utf-8').strip()
 os.environ['AZURE_OPENAI_ENDPOINT'] = os.getenv('endpoint')
 os.environ['AZURE_OPENAI_API_KEY'] = token
 
-# Set up your Azure OpenAI API key and Google Custom Search API credentials
-azure_openai_api_key = os.getenv("AZURE_OPENAI_API_KEY")
-azure_openai_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-google_api_key = os.getenv("googleapikey")
-google_cse_id = os.getenv("googlecseid")
-name=os.getenv("name")
-ver=os.getenv("ver")
-cert=os.getenv("cert")
 
-logging.basicConfig(
-    stream=sys.stdout, level=logging.INFO
-)  # logging.DEBUG for more verbose output
-logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-# Initialize the AzureOpenAI client
-llm = AzureOpenAI(
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"), 
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
-    api_version=os.getenv("ver")
+client = AzureOpenAI(
+  api_key = os.getenv("AZURE_OPENAI_API_KEY"),  
+  api_version = os.getenv("ver"),
+  azure_endpoint =os.getenv("AZURE_OPENAI_ENDPOINT") 
 )
 
-
-embed_model = AzureOpenAIEmbedding(
-    model="YourAzureOpenAIEmbeddingModelName",
-    deployment_name="YourAzureOpenAIEmbeddingDeploymentName",
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-    api_version=1,
+response = client.embeddings.create(
+    input = "Your text string goes here",
+    model= "embed"
 )
 
-Settings.llm = llm
-Settings.embed_model = embed_model
+print(response.model_dump_json(indent=2))
 
-documents = SimpleDirectoryReader(
-    input_files=["0.txt"]
-).load_data()
-index = VectorStoreIndex.from_documents(documents)
-
-query = "What is most interesting about this essay?"
-query_engine = index.as_query_engine()
-answer = query_engine.query(query)
-
-print(answer.get_formatted_sources())
-print("query was:", query)
-print("answer was:", answer)
 
