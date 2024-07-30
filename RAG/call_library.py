2f46b21a10b848d9e7b82323f4cb6a1f5b8a8d27
ajiayi-debug <e0774928@u.nus.edu>
2024-07-30 10:56:24 +0800

    so erm cosine similiarity is not rly a good option as the text chunks are too long so... will need to document this

--- RAG/call_library.py
+++ RAG/call_library.py
@@ -20,7 +20,7 @@ def main():
     output=get_references(text)
     codable=ast.literal_eval(output)
 
-    
+    # turns out gpt 4o is better than cosine similiarity as the text chunks are still too long for accurate representation of vectors, so cosine similiarity is off. Still, we can keep the embbedded database for future references!
     
     dfs = []
     for code in codable:
