7bc40506d6031457a4b567724f971611d2b0bbb0
ajiayi-debug <e0774928@u.nus.edu>
2024-11-05 13:36:45 +0800

    did some testing it shud work

--- RAG/agentic_search_test.py
+++ RAG/agentic_search_test.py
@@ -6,12 +6,12 @@ client = MongoClient(uri, tls=True, tlsCAFile=certifi.where())
 db = client['data']
 
 
-duplicate_collection_with_drop(uri, db.name, "Agentic_sieved_RAG_new_support_nosupport_confidence", "test_agentic_search_valid")
-duplicate_collection_with_drop(uri, db.name, "new_chunked_noembed", "new_chunked_noembed_retry")
-duplicate_collection_with_drop(uri, db.name, "new_ref_found_Agentic", "new_paper_after_retry")
-duplicate_collection_with_drop(uri, db.name, "No_match_agentic_new_confidence", "No_match_agentic_new_confidence_retry")
-duplicate_collection_with_drop(uri, db.name, "no_match_confidence", "no_match3_confidence_retry")
-duplicate_collection_with_drop(uri, db.name, "top_5", "top_5_test")
+# duplicate_collection_with_drop(uri, db.name, "Agentic_sieved_RAG_new_support_nosupport_confidence", "test_agentic_search_valid")
+# duplicate_collection_with_drop(uri, db.name, "new_chunked_noembed", "new_chunked_noembed_retry")
+# duplicate_collection_with_drop(uri, db.name, "new_ref_found_Agentic", "new_paper_after_retry")
+# duplicate_collection_with_drop(uri, db.name, "No_match_agentic_new_confidence", "No_match_agentic_new_confidence_retry")
+# duplicate_collection_with_drop(uri, db.name, "no_match_confidence", "no_match3_confidence_retry")
+# duplicate_collection_with_drop(uri, db.name, "top_5", "top_5_test")
 
 #delete_documents_by_reference_text(uri,db.name,target_collection_name,'Primary lactose intolerance is the most common form.')
 
