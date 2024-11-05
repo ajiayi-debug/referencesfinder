8dff6ec43db66df47e3e2bf5a6b2cf43a05cec18
ajiayi-debug <e0774928@u.nus.edu>
2024-11-05 12:07:14 +0800

    edit cleaning function to account for statements with papers that only has negative sentiment and send to retry as well as forgot to use filtered df as valid df

--- RAG/agentic_search_test.py
+++ RAG/agentic_search_test.py
@@ -5,15 +5,16 @@ uri = os.getenv("uri_mongo")
 client = MongoClient(uri, tls=True, tlsCAFile=certifi.where())
 db = client['data']
 
-source_collection_name = "Agentic_sieved_RAG_new_support_nosupport_confidence"
-target_collection_name = "test_agentic_search_valid"
 
-duplicate_collection(uri, db.name, source_collection_name, target_collection_name)
+duplicate_collection_with_drop(uri, db.name, "Agentic_sieved_RAG_new_support_nosupport_confidence", "test_agentic_search_valid")
+duplicate_collection_with_drop(uri, db.name, "new_chunked_noembed", "new_chunked_noembed_retry")
+duplicate_collection_with_drop(uri, db.name, "new_ref_found_Agentic", "new_paper_after_retry")
+duplicate_collection_with_drop(uri, db.name, "No_match_agentic_new_confidence", "No_match_agentic_new_confidence_retry")
+duplicate_collection_with_drop(uri, db.name, "no_match_confidence", "no_match3_confidence_retry")
+duplicate_collection_with_drop(uri, db.name, "top_5", "top_5_test")
 
-delete_documents_by_reference_text(uri,db.name,target_collection_name,'Primary lactose intolerance is the most common form.')
+#delete_documents_by_reference_text(uri,db.name,target_collection_name,'Primary lactose intolerance is the most common form.')
 
-agentic_search(collection_processed_name='new_chunked_noembed_retry',new_ref_collection='new_paper_after_retry',valid_collection_name='test_agentic_search_valid',invalid_collection_name='No_match_agentic_new_confidence_retry',not_match='no_match3_confidence_retry',threshold=76)
+agentic_search(collection_processed_name='new_chunked_noembed_retry',new_ref_collection='new_paper_after_retry',valid_collection_name='test_agentic_search_valid',invalid_collection_name='No_match_agentic_new_confidence_retry',not_match='no_match3_confidence_retry',top_5='top_5_test',threshold=76)
 
-#test case for a statement where no papers found.
-
-#need to mimic a statement where papers found are unsatisfactory now
+#test script for agentic search function
