a155c251ceefa763185f19679d61af84a48da651
ajiayidebug <e0774928@u.nus.edu>
2024-12-09 19:33:45 +0800

    changes to tailwind css and requirements.txt

--- update_article/tailwind.config.js
+++ update_article/tailwind.config.js
@@ -1,12 +1,11 @@
-// tailwind.config.js
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path if your files are located differently
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+/*eslint-env node*/
+/**@type{import('tailwindcss').Config} */
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path if your files are located differently
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
 
 
